8600e8bd340ff7ebebdabf6f576a970f0760e7e4
amerjali2022 <jose.ahumada.mt@usa.edu.co>
2022-09-30 19:43:31 -0500

    Version Inicial

---
+++ usa/src/main/java/co/edu/usa/modelo/CostumeCrudRepository.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.usa.modelo;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author JaliNet
+ */
+public interface CostumeCrudRepository extends CrudRepository<Costume, Integer>{
+    
+}
